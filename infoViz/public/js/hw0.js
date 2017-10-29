a000307cc34b1ea6d1fb7ed66e21d5f45cf5c67b
juanka588 <jcrodriguezd@unal.edu.co>
2017-10-29 13:31:03 +0100

    finish with dynamic ticks

--- infoViz/public/js/hw0.js
+++ infoViz/public/js/hw0.js
@@ -15,7 +15,10 @@ var f = d3.format(s);
 d3.tsv("./data/anscombe.tsv", function (error, data) {
     if (error)
         throw error;
-
+    var dataS = data.sort(function (a, b) {
+        return +a.x - +b.x;
+    });
+    console.log(dataS);
     for (var k in divs) {
         var div = divs[k];
         var table = div.append('table');
@@ -31,7 +34,7 @@ d3.tsv("./data/anscombe.tsv", function (error, data) {
                 .text('y');
 
         var row = table.selectAll('row')
-                .data(data)
+                .data(dataS)
                 .enter().filter(function (d) {
             return d.dataset == k;
         }).append('tr');
@@ -50,7 +53,7 @@ d3.tsv("./data/anscombe.tsv", function (error, data) {
                 .text(function (d) {
                     return f(d.y);
                 });
-        createGraph(data, k, chartContainer);
+        createGraph(dataS, k, chartContainer);
     }
     ;
 });
@@ -58,50 +61,50 @@ d3.tsv("./data/anscombe.tsv", function (error, data) {
 function createGraph(data, k, container) {
     var margin = {top: 30, right: 20, bottom: 30, left: 50},
             width = 600 - margin.left - margin.right,
-            height = 270 - margin.top - margin.bottom;
+            height = 300 - margin.top - margin.bottom;
 // Set the ranges
-    var x = d3.scaleLinear().rangeRound([0, width]);
-    var y = d3.scaleLinear().rangeRound([height, 0]);
+    var xAxis = d3.scaleLinear().rangeRound([0, width]);
+    var yAxis = d3.scaleLinear().rangeRound([height, 0]);
 // Adds the svg canvas
     var svg = d3.select("body")
             .append("svg")
             .attr("width", width + margin.left + margin.right)
-            .attr("height", height + margin.top + margin.bottom)
-    var g = svg.append("g");
-
-
-    x.domain(d3.extent(data, function (d) {
-        return  parseFloat(d.x);
+            .attr("height", height + margin.top + margin.bottom);
+    var g = svg.append("g")
+            .attr("transform", "translate(" + margin.left + ",0)");
+    data.forEach(function (d) {
+        d.x = +d.x;
+        d.y = +d.y;
+    });
+    xAxis.domain(d3.extent(data, function (d) {
+        return  d.x;
     }));
-    y.domain(d3.extent(data, function (d) {
-        return  parseFloat(d.y);
+    yAxis.domain(d3.extent(data, function (d) {
+        return  d.y;
     }));
-    //add points
+
     var line = d3.line()
             .x(function (d) {
-                return x(parseFloat(d.x));
+                return xAxis(d.x);
             })
             .y(function (d) {
-                return y(parseFloat(d.y));
-            })
+                return yAxis(3 + 0.5 * d.x);
+            });
 
     // Add the X Axis
     svg.append("g")
             .attr("class", "x axis")
-            .attr("transform", "translate(0," + height + ")")
-            .call(d3.axisBottom(x));
+            .attr("transform", "translate(" + margin.left + "," + height + ")")
+            .call(d3.axisBottom(xAxis));
 
     // Add the Y Axis
     svg.append("g")
             .attr("class", "y axis")
-            .call(d3.axisLeft(y));
-
+            .attr("transform", "translate(" + margin.left + ",0)")
+            .call(d3.axisLeft(yAxis));
 
     g.append("path")
             .datum(data)
-            .filter(function (d) {
-                return d.dataset == k;
-            })
             .attr("fill", "none")
             .attr("stroke", "steelblue")
             .attr("stroke-linejoin", "round")
@@ -118,11 +121,19 @@ function createGraph(data, k, container) {
             .append("circle")
             .attr("class", "dots" + k)
             .attr("cx", function (d) {
-                return x(parseFloat(d.x));
+                return xAxis(d.x);
             })
             .attr("cy", function (d) {
-                return y(parseFloat(d.y));
+                return yAxis(d.y);
             })
-            .attr("r", 2);
+            .attr("r", 2)
+            .append("title")
+            .text(function (d) {
+                return d.observation;
+            })
+            .append("text")
+            .text(function (d) {
+                return d.observation;
+            });
 
 }
