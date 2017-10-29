9c151aea70a7719277bef2dfc95b4c421e923f6a
juanka588 <jcrodriguezd@unal.edu.co>
2017-10-29 18:48:57 +0100

    comple hw1 without colors

--- infoViz/public/js/hw0.js
+++ infoViz/public/js/hw0.js
@@ -1,6 +1,5 @@
 
 var body = d3.select("body");
-var chartContainer = document.getElementById("data_vis_container");
 var divs = {
     '1': body.append('div'),
     '2': body.append('div'),
@@ -12,12 +11,19 @@ var s = d3.formatSpecifier("f");
 s.precision = d3.precisionFixed(0.01);
 var f = d3.format(s);
 
+// Define the div for the tooltip
+var div = d3.select("body").append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0);
+
 d3.tsv("./data/anscombe.tsv", function (error, data) {
     if (error)
         throw error;
     var dataS = data.sort(function (a, b) {
         return +a.x - +b.x;
     });
+
+
     console.log(dataS);
     for (var k in divs) {
         var div = divs[k];
@@ -53,12 +59,13 @@ d3.tsv("./data/anscombe.tsv", function (error, data) {
                 .text(function (d) {
                     return f(d.y);
                 });
-        createGraph(dataS, k, chartContainer);
+
+        createGraph(dataS, k);
     }
     ;
 });
 
-function createGraph(data, k, container) {
+function createGraph(data, k) {
     var margin = {top: 30, right: 20, bottom: 30, left: 50},
             width = 600 - margin.left - margin.right,
             height = 300 - margin.top - margin.bottom;
@@ -72,16 +79,14 @@ function createGraph(data, k, container) {
             .attr("height", height + margin.top + margin.bottom);
     var g = svg.append("g")
             .attr("transform", "translate(" + margin.left + ",0)");
+    
     data.forEach(function (d) {
         d.x = +d.x;
         d.y = +d.y;
+        d.r = 4;
     });
-    xAxis.domain(d3.extent(data, function (d) {
-        return  d.x;
-    }));
-    yAxis.domain(d3.extent(data, function (d) {
-        return  d.y;
-    }));
+    xAxis.domain([0, 20]);
+    yAxis.domain([0, 20]);
 
     var line = d3.line()
             .x(function (d) {
@@ -126,14 +131,33 @@ function createGraph(data, k, container) {
             .attr("cy", function (d) {
                 return yAxis(d.y);
             })
-            .attr("r", 2)
-            .append("title")
-            .text(function (d) {
-                return d.observation;
+            .attr("r", function (d) {
+                return d.r;
             })
-            .append("text")
+            .on('mouseover', function (d) {
+                div.transition()
+                        .duration(200)
+                        .style("opacity", .9);
+                div.html("observation: " + d.observation)
+                        .style("left", (d3.event.pageX) + "px")
+                        .style("top", (d3.event.pageY - 28) + "px");
+
+                d3.select(this).select("circle").transition()
+                        .duration(750)
+                        .attr("r", function (d) {
+                            return d.r * 3;
+                        });
+            })
+            .on('mouseout', function (d) {
+                div.transition()
+                        .duration(500)
+                        .style("opacity", 0);
+            })
+            .append("svg:title")
             .text(function (d) {
-                return d.observation;
-            });
+                return "observation: " + d.observation;
+            })
+            ;
+
 
 }
