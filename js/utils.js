e86c67059eb4c033296913983c57095df33a1447
juanka588 <juanka588@gmail.com>
2017-12-26 17:26:14 +0100

    add images add gender chart add scholar level chart

--- js/utils.js
+++ js/utils.js
@@ -5,3 +5,11 @@ function loadScript(src) {
     var head = document.getElementsByTagName("head")[0];
     head.appendChild(newjs);
 }
+function downloadObjectAsJson(exportObj, exportName) {
+    var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(exportObj));
+    var downloadAnchorNode = document.createElement('a');
+    downloadAnchorNode.setAttribute("href", dataStr);
+    downloadAnchorNode.setAttribute("download", exportName + ".json");
+    downloadAnchorNode.click();
+    downloadAnchorNode.remove();
+}
--- js/votes.js
+++ js/votes.js
@@ -98,6 +98,8 @@ function deleteItem(field, op, value) {
 function drawChars(data) {
     d3.selectAll("svg").remove().exit();
     realVotersSVG(data);
+    genderSVG(data);
+    educationSVG(data);
     approvalSVG(data);
     evalSVG(data);
 }
@@ -109,7 +111,7 @@ function evalSVG(data) {
     if (filterList.toString() !== "") {
         title += " " + candidatesMap[filterList.elements[0].value].name;
     }
-    prepareBarChart(title, "#svg_container3", toArray(transformData, candidatesKeys), candidatesKeys, yDomain, showRealVoterItems);
+    prepareBarChart(title, "#svg_container2", toArray(transformData, candidatesKeys), candidatesKeys, yDomain, showRealVoterItems);
 }
 
 function approvalSVG(data) {
@@ -137,6 +139,44 @@ function realVotersSVG(data) {
     prepareBarChart(title, "#svg_container1", transformData, xDomain, yDomain, showRealVoterItems);
 }
 
+function genderSVG(data) {
+    var xDomain = ["F", "M", "NSPP"];
+    var transformData = transformNominalData(data, "SEXE");
+    var yDomain = [0, transformData.max];
+    var title = "Gender";
+    prepareBarChart(title,
+            "#svg_container1",
+            toArray(transformData, xDomain),
+            xDomain,
+            yDomain,
+            showItems,
+            "Sex",
+            "Count",
+            new SVGHolder(250, 300, "#svg_container1"
+                    , {top: 20, right: 20, bottom: 20, left: 20}
+            , {top: 20, right: 0, bottom: 60, left: 70})
+            );
+}
+
+function educationSVG(data) {
+    var xDomain = ["1", "2", "S", "NSPP"];
+    var transformData = transformNominalData(data, "ETUDE");
+    var yDomain = [0, transformData.max];
+    var title = "Scholar Level";
+    prepareBarChart(title,
+            "#svg_container1",
+            toArray(transformData, xDomain),
+            xDomain,
+            yDomain,
+            showItems,
+            "Level",
+            "Count",
+            new SVGHolder(250, 300, "#svg_container1"
+                    , {top: 20, right: 20, bottom: 20, left: 20}
+            , {top: 20, right: 0, bottom: 60, left: 70})
+            );
+}
+
 function transformApprovalData(data) {
     return transformData(data, "AV_");
 }
@@ -151,7 +191,7 @@ function transformEvalData(data) {
 }
 function transformData(data, prefix) {
     var filteredData = filterList.applyFilters(data);
-    var summary = {max: -1, sum: 0};
+    var summary = {max: -1, sum: 0, count: 0};
     var transform = filteredData.reduce(function (acc, d) {
         var key;
         for (var i = 0; i < candidatesKeys.length; i++) {
@@ -164,6 +204,7 @@ function transformData(data, prefix) {
                 acc.max = acc[key];
             }
             acc.sum = acc.sum + d[prefix + key];
+            acc.count = acc.count + 1;
         }
         return acc;
     }, summary);
@@ -184,11 +225,35 @@ function transformRealVotersData(data) {
     return series;
 }
 
-function prepareBarChart(title, containerID, data, xDomain, yDomain, itemInflater) {
-    var svgHolder = new SVGHolder(500, 300, containerID
-            , {top: 20, right: 20, bottom: 20, left: 20}
-    , {top: 20, right: 0, bottom: 60, left: 60}
-    );
+function transformNominalData(data, key) {
+    var filteredData = filterList.applyFilters(data);
+    var summary = {max: -1, sum: 0, count: 0};
+    var transform = filteredData.reduce(function (acc, d) {
+        var value = d[key];
+        if (typeof (acc[value]) == "undefined") {
+            acc[value] = 0;
+        }
+        acc[value] = acc[value] + 1;
+        if (acc[value] > acc.max) {
+            acc.max = acc[value];
+        }
+        return acc;
+    }, summary);
+    return transform;
+}
+
+function prepareBarChart(title,
+        containerID,
+        data,
+        xDomain,
+        yDomain,
+        itemInflater,
+        xTitle = "Candidates",
+        yTitle = "Votes",
+        svgHolder = new SVGHolder(500, 300, containerID
+                , {top: 20, right: 20, bottom: 20, left: 20}
+        , {top: 20, right: 0, bottom: 60, left: 70}
+        )) {
     svgHolder.addChartTitle(title);
 //    svgHolder.showDebug();
     var xAxis = d3.scaleBand()
@@ -198,7 +263,8 @@ function prepareBarChart(title, containerID, data, xDomain, yDomain, itemInflate
     var yAxis = d3.scaleLinear().rangeRound([svgHolder.graphHeight, 0]);
     xAxis.domain(xDomain);
     yAxis.domain(yDomain);
-    showAxis(svgHolder, xAxis, yAxis);
+    svgHolder.addBottomAxis(xAxis, xTitle);
+    svgHolder.addLeftAxis(yAxis, yTitle);
     itemInflater(svgHolder, data, xAxis, yAxis);
 }
 
@@ -235,6 +301,9 @@ function showItems(svgHolder, data, xAxis, yAxis) {
             .enter().append("g")
             .attr("class", "series")
             .selectAll("rect")
+            .data(function (d) {
+                return d;
+            }).enter()
             .append("rect")
             .attr("fill", function (d) {
                 var temp = candidatesMap[d.data["key"]];
@@ -254,13 +323,14 @@ function showItems(svgHolder, data, xAxis, yAxis) {
 
 function addListeners(element) {
     element.on("click", function (d) {
-        showDetails(d.data.key);
+        showDetails(d.data);
     })
             .on('mouseover', function (d) {
                 tooltipDiv.transition()
                         .duration(200)
                         .style("opacity", .9);
-                tooltipDiv.html("<span class='axis-title'>Count: </span> " + d[1]
+                tooltipDiv.html("<img class='round' src='./images/" + candidatesMap[d.data["key"]].image + "'>"
+                        + "<span class='axis-title'>Count: </span> " + d[1]
                         + "<br /> <span>Name: " + candidatesMap[d.data["key"]].name + "</span>")
                         .style("left", (d3.event.pageX) + "px")
                         .style("top", (d3.event.pageY - 28) + "px");
@@ -276,10 +346,6 @@ function addListeners(element) {
             });
 }
 
-function showAxis(svgHolder, xAxis, yAxis) {
-    svgHolder.addBottomAxis(xAxis, "Candidates");
-    svgHolder.addLeftAxis(yAxis, "Votes");
-}
 
 function toArray(obj, properties) {
     var arr = [];
@@ -297,10 +363,16 @@ function toArray(obj, properties) {
     return [arr];
 }
 
-function showDetails(id) {
-    filterList.elements = [];
-    filterList.addFilter({field: "VOTE", operation: "eq", value: id});
+function showDetails(element) {
+    filterList.addFilter({field: "VOTE", operation: "eq", value: element.key});
     drawChars(parsedData);
+}
+
+function downloadData() {
+    var filteredData = filterList.applyFilters(parsedData);
+    var exportData = [];
+    exportData["data"] = filteredData;
+    downloadObjectAsJson(exportData, "votes");
 }
 
 function StringValueOf(operator) {
@@ -324,70 +396,113 @@ function StringValueOf(operator) {
 
 function getCandidatesMap() {
     var map = [];
+    map["1"] = {
+        id: "1",
+        color: "#65DBFF",
+        name: "Primary",
+        image: "primary.png"
+    };
+    map["2"] = {
+        id: "2",
+        color: "#4299E8",
+        name: "Secondary",
+        image: "secondary.jpg"
+    };
+    map["S"] = {
+        id: "S",
+        color: "#4247E8",
+        name: "Superior",
+        image: "superior.png"
+    };
+    map["F"] = {
+        id: "F",
+        color: "#ee3d5d",
+        name: "Female",
+        image: "F.png"
+    };
+    map["M"] = {
+        id: "M",
+        color: "#187ec2",
+        name: "Male",
+        image: "M.png"
+    };
     map["NDA"] = {
         id: "NDA",
         color: "#0088c6",
-        name: "Nicolas Dupont-Aignan"
+        name: "Nicolas Dupont-Aignan",
+        image: "NDA.jpg"
     };
     map["MLP"] = {
         id: "MLP",
         color: "#83726d",
-        name: "Marine Le Pen"
+        name: "Marine Le Pen",
+        image: "MLP.jpg"
     };
     map["EM"] = {
         id: "EM",
         color: "#ffd850",
-        name: "Emmanuel Macron"
+        name: "Emmanuel Macron",
+        image: "EM.jpg"
     };
     map["BH"] = {
         id: "BH",
         color: "#f39dc7",
-        name: "Benoît Hamon"
+        name: "Benoît Hamon",
+        image: "BH.jpg"
     };
     map["NA"] = {
         id: "NA",
         color: "#a21700",
-        name: "Nathalie Arthaud"
+        name: "Nathalie Arthaud",
+        image: "NA.jpg"
     };
     map["PP"] = {
         id: "PP",
         color: "#f96f43",
-        name: "Philippe Pouton"
+        name: "Philippe Pouton",
+        image: "PP.jpg"
     };
     map["JC"] = {
         id: "JC",
         color: "#464a4c",
-        name: "Jacques Cheminade"
+        name: "Jacques Cheminade",
+        image: "JC.jpg"
     };
     map["JL"] = {
         id: "JL",
         color: "#cee9f8",
-        name: "Jean Lassalle"
+        name: "Jean Lassalle",
+        image: "JL.jpg"
     };
     map["JLM"] = {
         id: "JLM",
         color: "#de2707",
-        name: "Jean-Luc Mélenchon"
+        name: "Jean-Luc Mélenchon",
+        image: "JLM.jpg"
     };
     map["FA"] = {
         id: "FA",
         color: "#131413",
-        name: "François Asselineau"
+        name: "François Asselineau",
+        image: "FA.jpg"
     };
     map["FF"] = {
         id: "FF",
         color: "#75bbe2",
-        name: "François Fillon"
+        name: "François Fillon",
+        image: "FF.jpg"
     };
     map["B"] = {
         id: "B",
         color: "#B0B29C",
-        name: "White vote"
+        name: "White vote",
+        image: "B.jpg"
     };
     map["NSPP"] = {
         id: "NSPP",
         color: "#14CC47",
-        name: "No answer"
+        name: "No answer",
+        image: "NSPP.png"
     };
     return map;
 }
